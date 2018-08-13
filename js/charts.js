361e49ead8e19f1d617c394439e8c1fe768415d5
Jaren Callo <jaren.callo@sensis.com.au>
2018-08-13 23:05:32 +1000

    added container class to make the elements in the center of the page

--- js/charts.js
+++ js/charts.js
@@ -1,98 +1,101 @@
-AmCharts.makeChart( "industryChart", {
-  "type": "pie",
-  "theme": "light",
-  "dataProvider": [ {
-    "industry": "industry 1",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 2",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 3",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 4",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 5",
-    "value": Math.floor(Math.random() * 100)
-  } ],
-  "valueField": "value",
-  "titleField": "industry",
-  "balloon":{
-   "fixedPosition":true
-  },
-  "labelText": "",
-  "titles": [
-		{
-			"id": "Industry",
-			"text": "Industry"
-		}
-	]
-} );
+AmCharts.makeChart("industryChart", {
+    "hideCredits": true,
+    "type": "pie",
+    "theme": "light",
+    "dataProvider": [{
+        "industry": "industry 1",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 2",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 3",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 4",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 5",
+        "value": Math.floor(Math.random() * 100)
+    }],
+    "valueField": "value",
+    "titleField": "industry",
+    "balloon": {
+        "fixedPosition": true
+    },
+    "labelText": "",
+    "titles": [
+        {
+            "id": "Industry",
+            "text": "Industry"
+        }
+    ]
+});
 
-AmCharts.makeChart( "demographicsChart", {
-  "type": "pie",
-  "theme": "light",
-  "dataProvider": [ {
-    "industry": "industry 1",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 2",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 3",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 4",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 5",
-    "value": Math.floor(Math.random() * 100)
-  } ],
-  "valueField": "value",
-  "titleField": "industry",
-  "balloon":{
-   "fixedPosition":true
-  },
-  "labelText": "",
-  "titles": [
-		{
-			"id": "Demographics",
-			"text": "Demographics"
-		}
-	]
-} );
+AmCharts.makeChart("demographicsChart", {
+    "hideCredits": true,
+    "type": "pie",
+    "theme": "light",
+    "dataProvider": [{
+        "industry": "industry 1",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 2",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 3",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 4",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 5",
+        "value": Math.floor(Math.random() * 100)
+    }],
+    "valueField": "value",
+    "titleField": "industry",
+    "balloon": {
+        "fixedPosition": true
+    },
+    "labelText": "",
+    "titles": [
+        {
+            "id": "Demographics",
+            "text": "Demographics"
+        }
+    ]
+});
 
-AmCharts.makeChart( "revenueChart", {
-  "type": "pie",
-  "theme": "light",
-  "dataProvider": [ {
-    "industry": "industry 1",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 2",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 3",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 4",
-    "value": Math.floor(Math.random() * 100)
-  }, {
-    "industry": "industry 5",
-    "value": Math.floor(Math.random() * 100)
-  } ],
-  "valueField": "value",
-  "titleField": "industry",
-  "balloon":{
-   "fixedPosition":true
-  },
-  "labelText": "",
-  "titles": [
-		{
-			"id": "Revenue",
-			"text": "Revenue"
-		}
-	]
-} );
+AmCharts.makeChart("revenueChart", {
+    "hideCredits": true,
+    "type": "pie",
+    "theme": "light",
+    "dataProvider": [{
+        "industry": "industry 1",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 2",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 3",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 4",
+        "value": Math.floor(Math.random() * 100)
+    }, {
+        "industry": "industry 5",
+        "value": Math.floor(Math.random() * 100)
+    }],
+    "valueField": "value",
+    "titleField": "industry",
+    "balloon": {
+        "fixedPosition": true
+    },
+    "labelText": "",
+    "titles": [
+        {
+            "id": "Revenue",
+            "text": "Revenue"
+        }
+    ]
+});
